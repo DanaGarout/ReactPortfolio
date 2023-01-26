52bc422e026b6d4e60b8a0c24845fb28c556930e
DanaGarout <danagarout5@gmail.com>
2023-01-26 18:25:48 +0300

    more updates

--- src/components/About.js
+++ src/components/About.js
@@ -7,22 +7,36 @@ import { CiUser } from "react-icons/ci";
 
 export const About = () => {
   return (
-    <div className='about-page '>
+    <div className='about-container'>
+      <div className='flex-child'>
+      <img className="aboutbg" src={about} alt='about picture'></img>
+      </div>
+      <div className='flex-child'>
+      <h1 className='subtitle'><CiUser></CiUser>About Me</h1> 
+            <p className='about-paragraph'>My name is Dana Garout, and I am based in Dammam, 
+            Saudi Arabia. Studying computer science with a
+             focus on web and mobile development in my last year
+              at Goldsmiths, University of London.
+              Building websites and mobile applications is my main area of emphasis,
+               and frontend development is one of my main interests.</p>
+      </div>
+
       
-      <div className='about-bg flex-child-element'>
+      {/* <div className='about-bg float-child'>
         <img className="aboutbg" src={about} alt='about picture'></img>
         </div>
         
-        <div className='text-about flex-child-element'> 
+        <div className='text-about float-child'> 
             <h1 className='subtitle'><CiUser></CiUser>About Me</h1>
             
             <p className='about-paragraph'>My name is Dana Garout, and I am based in Dammam, 
             Saudi Arabia. Studying computer science with a
              focus on web and mobile development in my last year
               at Goldsmiths, University of London.
-              Building websites and mobile applications is my main area of emphasis, and frontend development is one of my main interests.</p>
+              Building websites and mobile applications is my main area of emphasis,
+               and frontend development is one of my main interests.</p>
               
-        </div>
+        </div> */}
         
         
         
